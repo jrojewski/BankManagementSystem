7ba74cdebfbdf5038bc35f249d76ff20cbca6861
Łukasz Zdunowski <zdunek369@gmail.com>
2020-10-25 20:32:40 +0100

    Main function added.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+* Bank Managment System 
+* Project for developing own skills and...
+* To have fun
+*/
+
+#include <iostream>
+
+int main() {
+    
+    std::cout << "Sup bro!?";
+    return 0;
+    
+}
